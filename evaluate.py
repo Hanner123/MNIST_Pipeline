12dc24bcdfbddedd5a7b8b4e9e87543024a27e44
HaKatt <haka@mail.uni-paderborn.de>
2025-02-27 14:13:22 +0100

    confusion matrix

--- evaluate.py
+++ evaluate.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import torch
 from train import MnistModel  # Modell importieren
 from torch.utils.data.dataloader import DataLoader
+from sklearn.metrics import confusion_matrix
+import csv
 
 
 #function that can move data and model to a chosen device. (wichtig für GPU-Nutzung, da standartmäßig oft die CPU verwendet wird - es gibt probleme, wenn manche berechnungen auf der GPU und manche auf der CPU laufen)
@@ -34,6 +36,9 @@ def evaluate_model(model, test_loader):
     total_loss = 0
     criterion = torch.nn.CrossEntropyLoss()  # Beispiel für eine Verlustfunktion
 
+    all_labels = []
+    all_preds = []
+
     with torch.no_grad():  # Keine Gradienten berechnen während der Evaluation
         for inputs, labels in test_loader:
             # wieder zurück auf die GPU schieben
@@ -48,15 +53,85 @@ def evaluate_model(model, test_loader):
 
             # Vorhersagen berechnen
             _, predicted = torch.max(outputs, 1)
-            print("pred: ", predicted[0:5])
-            print("lable: ",labels[0:5])
+            # print("pred: ", predicted[0:5])
+            # print("lable: ",labels[0:5])
+
+            # Sammle die Vorhersagen und Labels für Confusion Matrix
+            all_labels.extend(labels.cpu().numpy())
+            all_preds.extend(predicted.cpu().numpy())
+
             correct_predictions += (predicted == labels).sum().item()
             total_predictions += labels.size(0)
-
+    print("correct: ", correct_predictions)
+    print("total: ", total_predictions)
     accuracy = correct_predictions / total_predictions
     average_loss = total_loss / len(test_loader)
     print(f"Test Loss: {average_loss:.4f}, Test Accuracy: {accuracy:.4f}")
+
+    # Berechnung der Confusion Matrix, das ist eine 10x10 Matrix
+    cm = confusion_matrix(all_labels, all_preds)
+
+    # Speichern der Confusion Matrix als CSV
+    with open('confusion_matrix.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        
+        # Schreibe die Kopfzeile (Index der Klassen)
+        writer.writerow([f"True/Predicted"] + [str(i) for i in range(cm.shape[0])])
+        
+        # Schreibe jede Zeile der Confusion Matrix
+        for i in range(cm.shape[0]):
+            writer.writerow([str(i)] + cm[i].tolist())
+            
+    print("Confusion Matrix:")
+    print(cm)
+
+    metrics = {}
+    # Berechnung von Recall und Specificity
+    for x in range(10):
+        print("  ")
+        print("Metrics for Number: ", x)
+        TP = cm[x, x]  # True Positive
+        #print("TP:", TP)
+
+        # True Negative
+        TN = 0
+        for i in range(10):
+            TN = TN + cm[i, i]
+        TN = TN - cm[x, x]
+        #print("TN:", TN)
+
+        # False Positive
+        FP = 0
+        for i in range(10):
+            FP = FP+ cm[i, x]
+        FP = FP - cm[x, x]
+        #print("FP:", FP)
+
+        # False Negative
+        FN = 0
+        for i in range(10):
+            FN = FN + cm[x, i]
+        FN = FN - cm[x, x]
+        #print("FN:", FN)
+
+        # Recall (Sensitivität)
+        recall = TP / (TP + FN) if (TP + FN) > 0 else 0
+        # Specificity
+        specificity = TN / (TN + FP) if (TN + FP) > 0 else 0
+
+        print(f"Recall: {recall:.6f}")
+        print(f"Specificity: {specificity:.6f}")
+
+        metrics[x] = {
+        'Recall': round(recall, 6),
+        'Specificity': round(specificity, 6)
+        }
+
+    with open('metrics.json', 'w') as json_file:
+        json.dump(metrics, json_file, indent=4)
+
     return average_loss, accuracy
+
 
 
 test_data = torch.load("test_data.pt", map_location="cpu",weights_only=False)                                               # test daten in cpu laden
